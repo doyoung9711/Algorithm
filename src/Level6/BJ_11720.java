165d62d4b3158e20aedb332f5e43d0f887e2e862
doyoung9711 <65013716+doyoung9711@users.noreply.github.com>
2022-07-15 20:09:54 +0900

    07/15

--- src/Level6/BJ_11720.java
+++ src/Level6/BJ_11720.java
@@ -1,9 +1,26 @@
 package Level6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BJ_11720 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int result=0;
+			//StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(br.readLine());
+			String st = br.readLine();
+			for(int i=0;i<N;i++) {
+				result = result+st.charAt(i)-'0';
+			}
+			System.out.println(result);
+			} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
